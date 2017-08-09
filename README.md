7afaa7617b45b4bc390bc646dd2ee231bff18151
huyaolong <huyaolong@yongche.com>
2017-08-09 21:21:54 +0800

    modify install

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 * **linux/mac:**
    1. Via curl: 
-   > curl -fsSL 'https://raw.githubusercontent.com/HaroldHoo/vimrc/master/install.sh' | bash
+   > bash -c "$(curl -fsSL 'https://raw.githubusercontent.com/HaroldHoo/vimrc/master/install.sh')"
 
    2. Via wget:
-   > wget 'https://raw.githubusercontent.com/HaroldHoo/vimrc/master/install.sh' -O - | bash
+   > bash -c "$(wget 'https://raw.githubusercontent.com/HaroldHoo/vimrc/master/install.sh' -O -)"
--- install.sh
+++ install.sh
@@ -6,12 +6,25 @@
 # date: 2017-08-02
 ##############################################
 
-echo -e "\033[33mCloning Vundle...\033[0m"
+printf "\033[33mCloning Vundle...\033[0m\n"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >/dev/null 2>&1
-echo -e "\033[33mDownloading color molokai...\033[0m"
+printf "\033[33mDownloading color molokai...\033[0m\n"
 mkdir -p ~/.vim/colors && curl -sSL 'https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim' -o ~/.vim/colors/molokai.vim
-echo -e "\033[33mDownloading .vimrc...\033[0m"
+printf "\033[33mDownloading .vimrc...\033[0m\n"
 curl -sSL 'https://raw.githubusercontent.com/HaroldHoo/vimrc/master/vimrc' -o ~/.vimrc
-echo -e ""
-read -p "Press Enter to install plugins for vim. [Enter] to continue or [Ctrl+c] to cancel."
-vim +PluginInstall
+
+printf "\n"
+read -r -p "Press [y] to install plugins for vim. Or [n] to run [vim +PluginInstall] by yourself. [Y/n] " input
+case $input in
+    [yY][eE][sS]|[yY])
+        vim +PluginInstall
+        ;;
+
+    [nN][oO]|[nN])
+        exit 1;
+        ;;
+
+    *)
+        vim +PluginInstall
+        ;;
+esac
