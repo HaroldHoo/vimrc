ae90709ee6687cf7e049c6c2086c8270d12e4bb5
huyaolong <huyaolong@yongche.com>
2017-10-27 16:49:25 +0800

    add neovim config

--- neovim.install.sh
+++ neovim.install.sh
@@ -7,17 +7,18 @@
 ##############################################
 
 type php >/dev/null 2>&1 || (echo "command not found: php"; exit 1;)
+type composer >/dev/null 2>&1 || (echo "command not found: composer"; exit 1;)
 
 if [ -z "$(php -v | grep 'PHP 7')" ]; then
-	echo "php version can not < 7"; exit 1;
+    echo "php version can not < 7"; exit 1;
 fi
 
 if [ -z "$(php -m | grep xml)" ]; then
-	echo "please install php-ext xml"; exit 1;
+    echo "please install php-ext xml"; exit 1;
 fi
 
 if [ -z "$(php -m | grep zip)" ]; then
-	echo "please install php-ext zip"; exit 1;
+    echo "please install php-ext zip"; exit 1;
 fi
 
 printf "\033[33mGet Plug...\033[0m\n"
@@ -34,8 +35,8 @@ printf "\n"
 read -r -p "Press [y] to install plugins for vim. Or [n] to run [vim +PluginInstall] by yourself. [Y/n] " input
 case $input in
     [yY][eE][sS]|[yY])
-	nvim +PlugInstall +UpdateRemotePlugins +qa
-	cd ~/.config/nvim/plugged/LanguageServer-php-neovim && composer run-script parse-stubs --working-dir=vendor/felixfbecker/language-server
+        nvim +PlugInstall +UpdateRemotePlugins +qa && \
+        cd ~/.config/nvim/plugged/LanguageServer-php-neovim && composer run-script parse-stubs --working-dir=vendor/felixfbecker/language-server
         ;;
 
     [nN][oO]|[nN])
@@ -43,7 +44,8 @@ case $input in
         ;;
 
     *)
-	nvim +PlugInstall +UpdateRemotePlugins +qa
-	cd ~/.config/nvim/plugged/LanguageServer-php-neovim && composer run-script parse-stubs --working-dir=vendor/felixfbecker/language-server
+        nvim +PlugInstall +UpdateRemotePlugins +qa && \
+        cd ~/.config/nvim/plugged/LanguageServer-php-neovim && composer run-script parse-stubs --working-dir=vendor/felixfbecker/language-server
         ;;
 esac
+
