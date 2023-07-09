976b1f7dfc1d1d3d8da59c5df2c3e5ffdb910e40
胡耀龙 <mail@yaolong.net>
2023-07-09 13:09:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@
 printf "\033[33mCloning Vundle...\033[0m\n"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim >/dev/null 2>&1
 printf "\033[33mDownloading color molokai...\033[0m\n"
-mkdir -p ~/.vim/colors && curl -sSL 'https://raw.fastgit.org/tomasr/molokai/master/colors/molokai.vim' -o ~/.vim/colors/molokai.vim
+mkdir -p ~/.vim/colors && curl -sSL 'https://raw.gitmirror.com/tomasr/molokai/master/colors/molokai.vim' -o ~/.vim/colors/molokai.vim
 printf "\033[33mDownloading .vimrc...\033[0m\n"
-curl -sSL 'https://raw.fastgit.org/HaroldHoo/vimrc/master/vimrc' -o ~/.vimrc
+curl -ksSL 'https://raw.gitmirror.com/HaroldHoo/vimrc/master/vimrc' -o ~/.vimrc
 
 printf "\n"
 read -r -p "Press [y] to install plugins for vim. Or [n] to run [vim +PluginInstall] by yourself. [Y/n] " input
